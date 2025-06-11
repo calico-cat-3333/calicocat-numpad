24da52651ca2346504bf428cc195cb52036fed7d
Calico cat <ahsdhmzyn@163.com>
2025-06-11 22:38:10 +0800

    disable cdc and storage by default

--- boot.py
+++ boot.py
@@ -8,9 +8,9 @@ bootcfg(
     # optional:
     source = board.GP27,
     boot_device = 0,
-    cdc_console = True,
+    cdc_console = False,
     cdc_data = False,
     midi = False,
     nkro = True,
-    storage = True,
+    storage = False,
 )
